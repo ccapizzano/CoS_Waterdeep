450ca36b9a4e3798c916d614bf5379d725298800
ccapizzano <ccapizzano@gmail.com>
2023-08-01 00:58:17 -0400

    Change main.js

--- main.js
+++ main.js
@@ -62,12 +62,6 @@ function init() {
 
   // Use PapaParse to load data from Google Sheets
   // And call the respective functions to add those to the map.
-  // Papa.parse(geomURL, {
-  //   download: true,
-  //   header: true,
-  //   complete: addGeoms,
-  // });
-
   Papa.parse(pointsURL, {
     download: true,
     header: true,
@@ -75,197 +69,71 @@ function init() {
   });
 
 
-  // /*
-  //  * Expects a JSON representation of the table with properties columns
-  //  * and a 'geometry' column that can be parsed by parseGeom()
-  //  */
-  // function addGeoms(data) {
-  //   data = data.data;
-  //   // Need to convert the PapaParse JSON into a GeoJSON
-  //   // Start with an empty GeoJSON of type FeatureCollection
-  //   // All the rows will be inserted into a single GeoJSON
-  //   let fc = {
-  //     type: "FeatureCollection",
-  //     features: [],
-  //   };
-
-  //   for (let row in data) {
-  //     // The Sheets data has a column 'include' that specifies if that row should be mapped
-  //     if (data[row].include == "y") {
-  //       let features = parseGeom(JSON.parse(data[row].geometry));
-  //       features.forEach((el) => {
-  //         el.properties = {
-  //           name: data[row].name,
-  //           description: data[row].description,
-  //         };
-  //         fc.features.push(el);
-  //       });
-  //     }
-  //   }
-
-  //   // The geometries are styled slightly differently on mouse hovers
-  //   let geomStyle = { color: "#2ca25f", fillColor: "#99d8c9", weight: 2 };
-  //   let geomHoverStyle = { color: "green", fillColor: "#2ca25f", weight: 3 };
-
-  //   L.geoJSON(fc, {
-  //     onEachFeature: function (feature, layer) {
-  //       layer.on({
-  //         mouseout: function (e) {
-  //           e.target.setStyle(geomStyle);
-  //         },
-  //         mouseover: function (e) {
-  //           e.target.setStyle(geomHoverStyle);
-  //         },
-  //         click: function (e) {
-  //           // This zooms the map to the clicked geometry
-  //           // Uncomment to enable
-  //           // map.fitBounds(e.target.getBounds());
-
-  //           // if this isn't added, then map.click is also fired!
-  //           L.DomEvent.stopPropagation(e);
-
-  //           document.getElementById("sidebar-title").innerHTML =
-  //             e.target.feature.properties.name;
-  //           document.getElementById("sidebar-content").innerHTML =
-  //             e.target.feature.properties.description;
-  //           sidebar.open(panelID);
-  //         },
-  //       });
-  //     },
-  //     style: geomStyle,
-  //   }).addTo(map);
-  // }
-
-  /*
-  //  * addPoints is a bit simpler, as no GeoJSON is needed for the points
-   */
+  //  addPoints is a bit simpler, as no GeoJSON is needed for the points
   function addPoints(data) {
     data = data.data;
-
-    //create a layer group to add the points to
     let pointGroupLayer = L.layerGroup().addTo(map);
+
+    // Choose marker type. Options are:
+    // (these are case-sensitive, defaults to marker!)
+    // marker: standard point with an icon
+    // circleMarker: a circle with a radius set in pixels
+    // circle: a circle with a radius set in meters
     let markerType = "marker";
+
+    // Marker radius
+    // Wil be in pixels for circleMarker, metres for circle
+    // Ignore for point
     let markerRadius = 100;
 
     for (let row = 0; row < data.length; row++) {
       let marker;
-      marker = L.marker([data[row].lat, data[row].lon]);
-
-      //add custom icons from Google Sheet CSV
-      console.log(data[row].color);
-      console.log(data[row].icon);
-
-      let icon = L.AwesomeMarkers.icon({
-        icon: data[row].icon,
-        markerColor: data[row].color,
-        prefix: "fa"
-      });
-      marker.setIcon(icon);
+      if (markerType == "circleMarker") {
+        marker = L.circleMarker([data[row].lat, data[row].lon], {
+          radius: markerRadius,
+        });
+      } else if (markerType == "circle") {
+        marker = L.circle([data[row].lat, data[row].lon], {
+          radius: markerRadius,
+        });
+      } else {
+        marker = L.marker([data[row].lat, data[row].lon]);
+      }
       marker.addTo(pointGroupLayer);
+
+      // UNCOMMENT THIS LINE TO USE POPUPS
+      //marker.bindPopup('<h2>' + data[row].name + '</h2>There's a ' + data[row].description + ' here');
+
+      // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
+      marker.feature = {
+        properties: {
+          name: data[row].name,
+          description: data[row].description,
+        },
+      };
+      marker.on({
+        click: function (e) {
+          L.DomEvent.stopPropagation(e);
+          document.getElementById("sidebar-title").innerHTML =
+            e.target.feature.properties.name;
+          document.getElementById("sidebar-content").innerHTML =
+            e.target.feature.properties.description;
+          sidebar.open(panelID);
+        },
+      });
+      // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
+
+      // AwesomeMarkers is used to create fancier icons
+      let icon = L.AwesomeMarkers.icon({
+        icon: "info-circle",
+        iconColor: "white",
+        markerColor: data[row].color,
+        prefix: "fa",
+        extraClasses: "fa-rotate-0",
+      });
+      if (!markerType.includes("circle")) {
+        marker.setIcon(icon);
+      }
     }
   }
 }
-
-// function addPoints(data) {
-//   data = data.data;
-//   let pointGroupLayer = L.layerGroup()
-
-//   // Choose marker type. Options are:
-//   // (these are case-sensitive, defaults to marker!)
-//   // marker: standard point with an icon
-//   // circleMarker: a circle with a radius set in pixels
-//   // circle: a circle with a radius set in meters
-//   let markerType = "marker";
-
-//   // Marker radius
-//   // Wil be in pixels for circleMarker, metres for circle
-//   // Ignore for point
-//   let markerRadius = 100;
-
-//   for (let row = 0; row < data.length; row++) {
-//     let marker;
-//     if (markerType == "circleMarker") {
-//       marker = L.circleMarker([data[row].lat, data[row].lon], {
-//         radius: markerRadius,
-//       });
-//     } else if (markerType == "circle") {
-//       marker = L.circle([data[row].lat, data[row].lon], {
-//         radius: markerRadius,
-//       });
-//     } else {
-//       marker = L.marker([data[row].lat, data[row].lon]);
-//     }
-//     marker.addTo(pointGroupLayer);
-
-//     // UNCOMMENT THIS LINE TO USE POPUPS
-//     //marker.bindPopup('<h2>' + data[row].name + '</h2>There's a ' + data[row].description + ' here');
-
-//     // COMMENT THE NEXT GROUP OF LINES TO DISABLE SIDEBAR FOR THE MARKERS
-//     marker.feature = {
-//       properties: {
-//         name: data[row].name,
-//         description: data[row].description,
-//       },
-//     };
-//     marker.on({
-//       click: function (e) {
-//         L.DomEvent.stopPropagation(e);
-//         document.getElementById("sidebar-title").innerHTML =
-//           e.target.feature.properties.name;
-//         document.getElementById("sidebar-content").innerHTML =
-//           e.target.feature.properties.description;
-//         sidebar.open(panelID);
-//       },
-//     });
-//     // COMMENT UNTIL HERE TO DISABLE SIDEBAR FOR THE MARKERS
-
-//     // AwesomeMarkers is used to create fancier icons
-//     let icon = L.AwesomeMarkers.icon({
-//       icon: "info-circle",
-//       iconColor: "white",
-//       markerColor: data[row].color,
-//       prefix: "fa",
-//       extraClasses: "fa-rotate-0",
-//     });
-//     if (!markerType.includes("circle")) {
-//       marker.setIcon(icon);
-//     }
-//   }
-// }
-
-// /*
-//  * Accepts any GeoJSON-ish object and returns an Array of
-//  * GeoJSON Features. Attempts to guess the geometry type
-//  * when a bare coordinates Array is supplied.
-//  */
-// function parseGeom(gj) {
-//   // FeatureCollection
-//   if (gj.type == "FeatureCollection") {
-//     return gj.features;
-//   }
-
-//   // Feature
-//   else if (gj.type == "Feature") {
-//     return [gj];
-//   }
-
-//   // Geometry
-//   else if ("type" in gj) {
-//     return [{ type: "Feature", geometry: gj }];
-//   }
-
-//   // Coordinates
-//   else {
-//     let type;
-//     if (typeof gj[0] == "number") {
-//       type = "Point";
-//     } else if (typeof gj[0][0] == "number") {
-//       type = "LineString";
-//     } else if (typeof gj[0][0][0] == "number") {
-//       type = "Polygon";
-//     } else {
-//       type = "MultiPolygon";
-//     }
-//     return [{ type: "Feature", geometry: { type: type, coordinates: gj } }];
-//   }
-// }
